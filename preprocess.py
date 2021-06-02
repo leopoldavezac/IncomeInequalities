f0515407f0f9fd4110da893eb3f95e43c3e670cf
leopoldavezac <leopoldavezacgmail.com>
2021-06-02 14:34:45 +0200

    (feat) - map country code to country nm for chart

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-FILE_PATH = "./WID_DATA_02062021-090326.csv"
+FILE_PATH = "./WID_DATA_02062021-120038.csv"
 
 
 def load():
@@ -19,7 +19,7 @@ def load():
     return df
 
 
-def clean(df):
+def clean_value_field(df):
 
     def convert_to_float(var):
         try:
@@ -35,12 +35,52 @@ def clean(df):
     return df
 
 
+def add_country_code(df):
+
+    df = df.copy(deep=True)
+
+    country_nms_to_drop = [
+        'Macao', 'Myanmar', 'Palestine', 'Zanzibar'
+        ]
+
+    df = df.loc[~df.country.isin(country_nms_to_drop)]
+
+    df.country.replace({
+        "Korea":"Korea, South",
+        "North Korea":"Korea, North",
+        "Viet Nam":"Vietnam",
+        "USA":"United States",
+        "Brunei Darussalam":"Brunei",
+        "Cote d’Ivoire":"Cote d'Ivoire",
+        "Lao PDR":"Laos",
+        "Syrian Arab Republic":"Syria",
+        "Russian Federation":"Russia"
+        }, inplace=True)
+
+    country_nm_to_code_df = pd.read_csv(
+        'https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv',
+        usecols=["COUNTRY", "CODE"]
+        )
+    country_nm_to_code_df.columns = [col_nm.lower() for col_nm in country_nm_to_code_df.columns]
+    country_nm_to_code_df.country.replace({
+        "Bahamas, The":"Bahamas",
+        "Congo, Republic of the":"Congo",
+        "Congo, Democratic Republic of the": "DR Congo",
+        "Gambia, The": "Gambia",
+
+    }, inplace=True)
+
+    df = df.merge(country_nm_to_code_df, on="country", how="left")
+
+    return df
+
 
 
 if __name__ == "__main__":
 
     df = load()
-    df = clean(df)
+    df = clean_value_field(df)
+    df = add_country_code(df)
     df.to_csv("WID_clean.csv", index=False)
 
 
